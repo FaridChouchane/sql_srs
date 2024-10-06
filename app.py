b6ca358ffed0f3b3513c93efc7f808a878d33142
root <root@Fa>
2024-10-06 15:58:37 +0200

    correction du bug quand la zone de saisie est vide

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
     sql_query = st.text_area(label = "Entrez votre input : ")
-    result = duckdb.query(sql_query).df()
-    st.write(f"Vous avez entré la query suivante : {sql_query}")
-    st.dataframe(result)
 
+    if sql_query:
+        try:
+            # Exécuter la requête SQL
+            result = duckdb.query(sql_query).df()
+            st.write(f"Vous avez entré la query suivante : {sql_query}")
+            st.dataframe(result)  # Afficher le résultat sous forme de tableau
+        except Exception as e:
+            # Afficher une erreur si la requête échoue
+            st.error(f"Erreur lors de l'exécution de la requête : {e}")
+    else:
+        st.info("Veuillez entrer une requête SQL.")  # Message si aucune requête n'est entrée
